f11b658129e0bcdd60bfa2773605bb6c8b88d98e
Brian Hu <55677478+BriannHu@users.noreply.github.com>
2023-10-25 18:26:07 -0400

    Revert "merge back-end with main"

---
+++ backend/src/app.js
@@ -0,0 +1,36 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import { Pool } from "pg";
+import { fakeGETPin } from "../routes";
+
+// https://dev.to/chandrapantachhetri/docker-postgres-node-typescript-setup-47db
+
+const app = express();
+dotenv.config(); //Reads .env file and makes it accessible via process.env
+
+app.get("/test", (req, res, next) => {
+  res.send("hi");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at ${process.env.PORT}`);
+});
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432")
+});
+
+const connectToDB = async () => {
+  try {
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectToDB();
+
+fakeGETPin();
