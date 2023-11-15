304dc3b2474115757a856423b20ecec5b24a2cd2
KGsxj <102866803+KGsxj@users.noreply.github.com>
2023-11-15 18:33:42 -0500

    drafted delete pin route

--- api/src/pin/pin.controller.ts
+++ api/src/pin/pin.controller.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@decorators/di';
-import { Controller, Get, Response } from '@decorators/express';
+import { Controller, Delete, Get, Response, Params } from '@decorators/express';
 import { Response as ExpressResponse } from 'express';
 import PinService from './pin.service';
 import { ManyPinResponseDto, PinDto, PinResponseDto } from './pin.dto';
@@ -21,6 +21,36 @@ class PinController {
       ? response.status(200).send(dto)
       : response.status(404).send(dto);
   }
+
+  @Delete('/:id') 
+    async deleteByID(@Response() response: ExpressResponse, id: string) {
+      const result: PinDto = await this.service.remove(Number(id));
+
+      const dto: PinResponseDto = result
+        ? ({ status: 'Success', pin: result } as PinResponseDto)
+        : ({ status: 'Error'} as PinResponseDto);
+
+        return result 
+        ? response.status(200).send(dto)
+        : response.status(404).send(dto);
+    }
+  }
+  
+
+  // @Get('/id')
+  // async getByID(@Response() response: ExpressResponse, pin_ID: string) {
+  //   const result: PinDto = await this.service.findUnique(pin_ID)
+
+  //   const dto: PinResponseDto = result
+  //     ? ({ status: 'Success', pin: result } as PinResponseDto)
+  //     : ({ status: 'Error '} as PinResponseDto)
+
+  //   return result
+  //   ? response.status(200).send(dto)
+  //   : response.status(404).send(dto);
+  // }
+  
+
 }
 
 export { PinController };
--- api/src/pin/pin.service.ts
+++ api/src/pin/pin.service.ts
@@ -8,6 +8,34 @@ class PinService {
   async findMany() {
     return await this.prisma.pin.findMany();
   }
+
+  // async findUnique(pin_ID: string) {
+  //   return await this.prisma.pin.findUnique({
+  //     where: {
+  //       name: pin_ID
+  //     }
+  //   })
+  // }
+
+  async remove(id: number) {
+    return await this.prisma.pin.delete({
+      where: {
+        id: id
+      }
+    })
+  }
+
+    // Delete a Pin.
+    // @param {PinDeleteArgs} args - Arguments to delete one Pin.
+    // @example
+    // // Delete one Pin
+    // const Pin = await prisma.pin.delete({
+    // where: {
+    // // ... filter to delete one Pin
+    // }
+    // })
 }
+
+
 
 export default PinService;
