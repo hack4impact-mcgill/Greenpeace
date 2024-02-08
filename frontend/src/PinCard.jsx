93fa3748de9a02fc760a28635a924184217a4ff1
Yu An Lu <yu.a.lu@mail.mcgill.ca>
2024-02-07 20:01:17 -0500

    added map icon and name to cardheader

--- frontend/src/PinCard.jsx
+++ frontend/src/PinCard.jsx
@@ -3,18 +3,28 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import RoomIcon from '@material-ui/icons/Room';
 
 const PinCard = ({ date, name }) => {
   return (
     <Card variant="outlined">
       <CardHeader 
+          avatar={
+            <Avatar>
+              <RoomIcon/>
+            </Avatar>
+            
+          }
           action={
           <IconButton aria-label="settings">
-            <MoreVertIcon />
+            <MoreVertIcon/>
           </IconButton>
-        }/>
+          }
+          title="ADDED PIN"
+        />
       <CardContent>
         <Typography variant="h5" component="h2">
           {name}
