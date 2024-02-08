ad5045a30f9214287f39f1d1246601b21ed67da0
Annie <annie.gouchee@mail.mcgill.ca>
2024-02-07 19:04:44 -0500

    Styling

--- frontend/src/ChangeLog.jsx
+++ frontend/src/ChangeLog.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
+
 
 const useStyles = makeStyles({
-  list: {
-    width: 250,
+  drawer: {
+    width: 400
   },
-  fullList: {
-    width: 'auto',
+  header: {
+    height: 75,
+    backgroundColor: '#3E654A',
+    padding: 0,
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: 20
   },
+
+  // changeLogTitle: {
+  //   color: '#ffffff', // Set color to white
+  //   fontFamily: 'Roboto', // Set font family to Roboto
+  //   fontSize: '1.5rem', // Adjust font size as needed
+  //   fontWeight: 400, // Adjust font weight as needed
+  //   margin: 0, // Remove default margin
+  // },
+
 });
 
 export default function ChangeLog() {
@@ -39,7 +55,10 @@ export default function ChangeLog() {
         onOpen={toggleDrawer(true)}
       >
         <div className={classes.drawer}>
-          <h2>Change Log</h2>
+          <div className={classes.header}>
+            <Typography style={{ color: "white" }}>
+              Change Log            </Typography>
+          </div>
         </div>
       </SwipeableDrawer>
     </div>
