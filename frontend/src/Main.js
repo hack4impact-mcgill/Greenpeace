174f6c568b174bf4b5f361337b8ca38cf9115892
hanzili <hanzi.li@mail.mcgill.ca>
2024-03-01 01:02:44 -0500

    fix eslint

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -16,7 +16,6 @@ function Map() {
     const [selectedPin, setSelectedPin] = useState(null);
     const [pins, setPins] = useState([]);
     const [isListening, setIsListening] = useState(false);
-    const [map, setMap] = useState(null);
     const { isLoaded, loadError } = useJsApiLoader({
         id: "google-map-script",
         googleMapsApiKey: API_KEY,
@@ -36,12 +35,8 @@ function Map() {
     const onLoad = React.useCallback(function callback(map) {
         const bounds = new window.google.maps.LatLngBounds();
         map.fitBounds(bounds);
-        setMap(map);
-    });
+    }, []);
 
-    const onUnmount = React.useCallback(function callback(map) {
-        setMap(null);
-    });
 
     if (loadError) return <h1>{loadError}</h1>
     if (!isLoaded) return <h1>Loading</h1>;
@@ -49,44 +44,43 @@ function Map() {
     return (
         <GoogleMap
             defaultZoom={19}
-            defaultCenter={{ lat: 45.5048, lng: -73.5772}}
-            options={{styles: mapStyles}}
+            defaultCenter={{ lat: 45.5048, lng: -73.5772 }}
+            options={{ styles: mapStyles }}
             onLoad={onLoad}
-            onUnmount={onUnmount}
-            onClick={( event ) => {
+            onClick={(event) => {
                 if (isListening) {
                     createPin([event.latLng.lat(), event.latLng.lng()], "New Pin", "New Description");
                     setIsListening(false);
-                    }
                 }
-            }
+            }}
+            mapContainerStyle={{ height: "100%" }}
         >
             {pins.map(pin => (
                 <Marker
-                key={ pin.id }
-                position={{
-                    lat: pin.coordinates[0],
-                    lng: pin.coordinates[1]
-                }}
-                onClick={() => {
-                    setSelectedPin(pin);
-                }}
-                icon={{
-                    scaledSize: new window.google.maps.Size(70, 70)
-                }}
+                    key={pin.id}
+                    position={{
+                        lat: pin.coordinates[0],
+                        lng: pin.coordinates[1]
+                    }}
+                    onClick={() => {
+                        setSelectedPin(pin);
+                    }}
+                    icon={{
+                        scaledSize: new window.google.maps.Size(70, 70)
+                    }}
                 />
             ))}
 
             {selectedPin && (
                 <InfoWindow
-                onCloseClick={() => {
-                    setSelectedPin(null);
-                }}
-                position={{
-                    lat: selectedPin.coordinates[0],
-                    lng: selectedPin.coordinates[1]
-                }}
-                justify="center"
+                    onCloseClick={() => {
+                        setSelectedPin(null);
+                    }}
+                    position={{
+                        lat: selectedPin.coordinates[0],
+                        lng: selectedPin.coordinates[1]
+                    }}
+                    justify="center"
                 >
                     <div>
                         <h2>{selectedPin.location}</h2>
@@ -94,7 +88,7 @@ function Map() {
                         <Link
                             component="button"
                             variant="body2"
-                            onClick={() => {}}
+                            onClick={() => { }}
                         >
                             Expand
                         </Link>
@@ -102,30 +96,30 @@ function Map() {
                 </InfoWindow>
             )}
 
-            <Button 
-                color="primary" 
-                variant="contained" 
-                style={{ 
-                    zIndex: 1, 
-                    position: "absolute", 
+            <Button
+                color="primary"
+                variant="contained"
+                style={{
+                    zIndex: 1,
+                    position: "absolute",
                     top: "4vh",
                     right: "12vh"
-                }} 
+                }}
             >Filter</Button>
-            <Button 
-                color="primary" 
-                variant="contained" 
-                style={{ 
-                    zIndex: 1, 
-                    marginTop: "-15vh", 
-                    position: "absolute", 
-                    right: "12vh", 
-                    height: "65px", 
-                    width: "50px", 
-                    fontSize: "60px", 
-                    borderRadius: "50px", 
-                    fontWeight: "300" 
-                }} 
+            <Button
+                color="primary"
+                variant="contained"
+                style={{
+                    zIndex: 1,
+                    marginTop: "-15vh",
+                    position: "absolute",
+                    right: "12vh",
+                    height: "65px",
+                    width: "50px",
+                    fontSize: "60px",
+                    borderRadius: "50px",
+                    fontWeight: "300"
+                }}
                 onClick={() => {
                     setIsListening(true);
                 }}
@@ -142,13 +136,7 @@ export default function Main() {
         <HashRouter>
             <div style={{ width: "97vw", height: "100vh" }}>
 
-            <MapWrapped
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places}`}
-                loadingElement={<div style={{ height: `95%` }} />}
-                containerElement={<div style={{ height: `95%` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-                style={{ zIndex: -1 }}
-            />
+                <MapWrapped />
             </div>
         </HashRouter>
     );
